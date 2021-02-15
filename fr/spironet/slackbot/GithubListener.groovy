5311398da858a411ba884d1c48df684886d5c841
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2021-02-15 12:35:54 +0100

    github - search for all georchestra repositories

--- fr/spironet/slackbot/GithubListener.groovy
+++ fr/spironet/slackbot/GithubListener.groovy
@@ -27,25 +27,72 @@ class GithubListener implements SlackMessagePostedListener  {
     * prs <repository>: lists the opened PRs on the given repository
     examples:
     * `!github prs georchestra-rennes-configuration` will return the opened PRs on `camptocamp/georchestra-rennes-configuration`
+    * `!github prs geor` will return the opened PRs on each c2c georchstra repositories
     """
+
+    def  geor_repos = [
+        "terraform-georchestra",
+        "georchestra-rennes-configuration",
+        "georchestra-pigma-configuration",
+        "georchestra-georhena-configuration",
+        "georchestra-grandest-configuration",
+        "georchestra-hauteloire-ansible",
+        "georchestra-hauteloire-configuration",
+        "georchestra-hauteloire-docker",
+        "georchestra-drealcorse-ansible",
+        "georchestra-drealcorse-configuration",
+        "georchestra-drealcorse-docker",
+        "terraform-georchestra",
+        "terraform-rennesmetropole"
+    ]
 
     public GithubListener() {
        gh_login = System.getenv("GITHUB_USERNAME")
        gh_password = System.getenv("GITHUB_PASSWORD")
     }
 
+    private def getOpenedPrsFromGithub(def gh, def repo) {
+      def curRepo = gh.getRepository("camptocamp/${repo}")
+      return curRepo.getPullRequests(GHIssueState.OPEN)
+
+    }
+
+    private SlackPreparedMessage openedGeorchestraPrs() {
+      def issues = [:]
+      def gh = GitHub.connect(gh_login, gh_password)
+
+      geor_repos.each {
+        issues[it] = getOpenedPrsFromGithub(gh, it)
+      }
+
+      def ret = ""
+      def totalIssues = issues.collect { it.value.size()  }.sum()
+      if (totalIssues == 0) {
+        ret = "*No opened PRs on the georchestra repositories as for now.*"
+      } else {
+          issues.each { k,v ->
+              if (v.size() > 0) {
+                  ret += "List of opened PR on repository ${k}:\n"
+                      v.each {
+                          ret += "* ${it.getTitle()} - ${it.getHtmlUrl()} (author: ${it.getUser().getLogin()})\n"
+                      }
+              }
+              ret += "\n"
+          }
+      }
+      return new SlackPreparedMessage.Builder().withMessage(ret).build()
+    }
+
     private SlackPreparedMessage openedPrs(def repo) {
       def ret = "List of opened PR on repository ${repo}:\n"
 
       def gh = GitHub.connect(gh_login, gh_password)
-
-      def curRepo = gh.getRepository("camptocamp/${repo}")
-      def prs = curRepo.getPullRequests(GHIssueState.OPEN)
+      def prs = getOpenedPrsFromGithub(gh, repo)
       if (prs.size() == 0) {
         ret += "*none*\n"
       } else {
         prs.each { pr ->
-           ret += "* ${pr.getTitle()} - ${pr.getHtmlUrl()}\n"
+           ret += "* ${pr.getTitle()} - ${pr.getHtmlUrl()} (author: ${pr.getUser().getLogin()})\n"
         }
       }
       return new SlackPreparedMessage.Builder().withMessage(ret).build()
@@ -67,7 +114,11 @@ class GithubListener implements SlackMessagePostedListener  {
           def args = match[0][1].split(" ")
           if (args[0] == "prs") {
                 def repository = args[1]
-                session.sendMessage(channelOnWhichMessageWasPosted, openedPrs(repository))
+                if (repository == "geor") {
+                  session.sendMessage(channelOnWhichMessageWasPosted, openedGeorchestraPrs())
+                } else {
+                  session.sendMessage(channelOnWhichMessageWasPosted, openedPrs(repository))
+                }
           }
         } catch (Exception e) {
           logger.error("Error occured", e)
