5df469c530938ca4a4415204bd649052118b7bf6
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2021-09-06 22:09:01 +0200

    confluence - improving test coverage

--- src/test/groovy/fr/spironet/slackbot/confluence/ConfluenceRssTest.groovy
+++ src/test/groovy/fr/spironet/slackbot/confluence/ConfluenceRssTest.groovy
@@ -1,11 +1,12 @@
 package fr.spironet.slackbot.confluence
 
 import groovy.xml.XmlSlurper
+import org.junit.Assume
 import org.junit.Before
 import org.junit.Test
 
 import static org.junit.Assert.assertTrue
-
+import static org.junit.Assume.assumeTrue
 class ConfluenceRssTest {
 
     private def toTest
@@ -26,5 +27,28 @@ class ConfluenceRssTest {
     void testRss() {
         def items = toTest.rssLatestModifiedItems()
         assertTrue(items.size() == 8)
+    }
+
+    @Test(expected = RuntimeException)
+    void testConfluenceRssMissingEnvVariable() {
+        assumeTrue(System.env["JIRA_CLIENT_PROPERTY_FILE"] == null)
+
+        def toTest = new ConfluenceRss()
+    }
+
+    @Test
+    void testConfluenceRssGetRss() {
+        def toTest2 = new ConfluenceRss("/dev/null")
+        toTest2.http = new Object() {
+            def get(def args, def closure) {
+                def ret = new File(
+                        this.getClass().getResource("rssfeed.xml").toURI()
+                ).text
+                closure.call(null, new XmlSlurper().parseText(ret))
+            }
+        }
+        def ret = toTest2.getRssFeed()
+
+        assertTrue(ret[0].name == "feed")
     }
 }
